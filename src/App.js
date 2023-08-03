bf9979f0ec9590431939d1101a0c1897db757f8c
Shashank Kumar <111291676+shashankkr20@users.noreply.github.com>
2023-08-03 22:56:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   return (
   <>
    
-   <BrowserRouter basename='https://successful-onesies-goat.cyclic.app'>
+   <BrowserRouter basename=''>
    <Routes>
     <Route path='/' element={<Login/>}></Route>
     <Route path='/itemdet' element={<Itemdetails/>}></Route>
